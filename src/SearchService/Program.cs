e0638b9bb3f9cb3790e856d422ee36d5944d30a6
Pham Huu Truong <phamhuu.truong.7@gmail.com>
2024-01-26 08:54:43 +0700

    Add and configure Mass Transit

--- src/SearchService/Program.cs
+++ src/SearchService/Program.cs
@@ -1,3 +1,4 @@
+using MassTransit;
 using Polly;
 using Polly.Extensions.Http;
 using SearchService;
@@ -8,6 +9,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddHttpClient<AuctionSvcHttpClient>().AddPolicyHandler(GetPolicy());
+
+builder.Services.AddMassTransit(x =>
+{
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ConfigureEndpoints(context);
+    });
+});
 
 var app = builder.Build();
 
